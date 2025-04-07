df952884cd2c3eb20a6e85453c5886dd805c5c82
Mallik Ebadat <31974569+MK-IB@users.noreply.github.com>
2025-04-07 10:12:07 +0530

    Cube Sorting

--- Assets/_CakeMaster/_Scripts/GameplayRelated/GridManager.cs
+++ Assets/_CakeMaster/_Scripts/GameplayRelated/GridManager.cs
@@ -9,11 +9,14 @@ public class GridManager : MonoBehaviour
     private GameObject[,] grid;
     private Color[] colors = { Color.red, Color.blue }; // Two colors
     
-    public LineRenderer lineRenderer; // Assign a LineRenderer in the inspector
+    LineRenderer lineRenderer; // Assign a LineRenderer in the inspector
     private bool isSelecting = false;
+    [SerializeField] private Vector3 lineOffset;
+    [SerializeField] private Color lineStartColor, lineEndColor;
 
     void Start()
     {
+        lineRenderer = GetComponent<LineRenderer>();
         lineRenderer.positionCount = 0;
         GenerateGrid();
     }
@@ -65,11 +68,58 @@ public class GridManager : MonoBehaviour
         if (cube1 != null)
         {
             isSelecting = true;
+            lineRenderer.positionCount = 1;
+            lineRenderer.SetPosition(0, cube1.transform.position + lineOffset);
         }
     }
     void DetectSecondCube(Vector2 touchPosition)
     {
+        CubeController cube = GetCubeFromTouch(touchPosition);
+        if (cube != null && cube != cube1 && (cube1.Row != cube.Row && cube1.Column != cube.Column))
+        {
+            cube2 = cube;
+            //Debug.Log($"CUBE 2: {cube2}");
+            DetectOtherCubes();
+        }
+    }
+
+    void DetectOtherCubes()
+    {
+        cube3 = grid[cube1.Row, cube2.Column].GetComponent<CubeController>();
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetPosition(1, cube3.transform.position + lineOffset);
         
+        lineRenderer.positionCount = 3;
+        lineRenderer.SetPosition(2, cube2.transform.position + lineOffset);
+        
+        cube4 = grid[cube2.Row, cube1.Column].GetComponent<CubeController>();
+        lineRenderer.positionCount = 4;
+        lineRenderer.SetPosition(3, cube4.transform.position + lineOffset);
+        lineRenderer.positionCount = 5;
+        lineRenderer.SetPosition(4, cube1.transform.position + lineOffset);
+        VisualForSorting();
+    }
+
+    bool isSortingPossible = false;
+    void VisualForSorting()
+    {
+        Debug.Log("TRUEE");
+        if(cube1 == null || cube2 == null || cube3 == null || cube4 == null) return;
+        if (cube1.ColorKey == cube2.ColorKey &&
+            cube2.ColorKey == cube3.ColorKey &&
+            cube3.ColorKey == cube4.ColorKey)
+        {
+            
+            lineRenderer.startColor = lineStartColor;
+            lineRenderer.endColor = lineEndColor;
+            isSortingPossible = true;
+        }
+        else
+        {
+            lineRenderer.startColor = Color.grey;
+            lineRenderer.endColor = Color.grey;
+            isSortingPossible = false;
+        }
     }
     CubeController GetCubeFromTouch(Vector2 touchPosition)
     {
@@ -83,6 +133,23 @@ public class GridManager : MonoBehaviour
 
     void ResetSelection()
     {
-        
+        lineRenderer.positionCount = 0;
+        Debug.Log($"CUBE 1 POS: {cube1.Row}, {cube1.Column}");
+        Debug.Log($"CUBE 2 POS: {cube2.Row}, {cube2.Column}");
+        if (isSortingPossible)
+        {
+            int minRow = Mathf.Min(cube1.Row, cube2.Row, cube3.Row, cube4.Row);
+            int maxRow = Mathf.Max(cube1.Row, cube2.Row, cube3.Row, cube4.Row);
+            int minCol = Mathf.Min(cube1.Column, cube2.Column, cube3.Column, cube4.Column);
+            int maxCol = Mathf.Max(cube1.Column, cube2.Column, cube3.Column, cube4.Column);
+
+            for (int row = minRow; row <= maxRow; row++)
+            {
+                for (int col = minCol; col <= maxCol; col++)
+                {
+                    grid[row, col].SetActive(false);
+                }
+            }
+        }
     }
 }
